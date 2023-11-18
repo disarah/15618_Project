327cea24318282fa4b46f7b883710680ff3efef8
soleee99 <soleeep99@gmail.com>
2023-11-17 20:18:57 -0500

    cpu naive attention

---
+++ src/main.cpp
@@ -0,0 +1,104 @@
+#include <stdio.h>
+#include <iostream>
+#include <cmath>
+#include <chrono>
+#include <cstdlib>
+
+#include "naiveAttention.h"
+
+#define N 1024
+#define D_MODEL 1024
+#define N_HEAD 16    // should be able to divide D_MODEL
+#define d_k D_MODEL / N_HEAD
+
+class CPUNaiveAttention {
+  public:
+    
+    // float query[N_HEAD][N][d_k];
+    // float key[N_HEAD][N][d_k];
+    // float value[N_HEAD][N][d_k];
+
+    float *query;
+    float *key;
+    float *value;
+
+
+    CPUNaiveAttention() {
+        query = (float*)malloc(N_HEAD*N*d_k * sizeof(float));
+        key = (float*)malloc(N_HEAD*N*d_k* sizeof(float));
+        value = (float*)malloc(N_HEAD*N*d_k* sizeof(float));
+    }
+
+    int get(int h, int n, int d) {return h*N*d_k + n*d_k + d;}   // Implicitly inline
+
+    void scaled_dot_product_attention() {
+        float *attn_scores = (float*)malloc(N_HEAD*N*N* sizeof(float));
+        float *result = (float*)malloc(N_HEAD*N*d_k * sizeof(float));
+
+        // query x key^T
+        auto beg = std::chrono::high_resolution_clock::now();
+        float sqrt_d_k = sqrt(d_k);
+        float score;
+        for(int h = 0; h < N_HEAD; h++) {
+            for(int n1 = 0; n1 < N; n1++) {
+                for(int n2 = 0; n2 < N; n2++) {
+                    score = 0.0;
+                    for(int d=0; d < d_k; d++) {
+                        score += query[get(h,n1,d)] * key[get(h,n2,d)];
+                    }
+                    attn_scores[h*N*N + n1*N + n2] = score / sqrt_d_k;
+                }
+            }
+        }
+        auto end = std::chrono::high_resolution_clock::now();
+        std::cout << "query key: " << std::chrono::duration_cast<std::chrono::microseconds>(end - beg).count() << std::endl;
+
+
+        // softmax on attn_scores
+        std::cout << "softmax\n";
+        beg = std::chrono::high_resolution_clock::now();
+        for(int h = 0; h < N_HEAD; h++) {
+            for(int n1 = 0; n1 < N; n1++) {
+                float sum = 0.0;
+                for(int n2 = 0; n2 < N; n2++) {
+                    sum += attn_scores[h*N*N + n1*N + n2];
+                }
+                for(int n2 = 0; n2 < N; n2++) {
+                    attn_scores[h*N*N + n1*N + n2] /= sum;
+                }
+            }
+        }
+        end = std::chrono::high_resolution_clock::now();
+        std::cout << "softmax: " << std::chrono::duration_cast<std::chrono::nanoseconds>(end - beg).count() << std::endl;
+
+
+        // attn_scores x value
+        beg = std::chrono::high_resolution_clock::now();
+        for(int h = 0; h < N_HEAD; h++) {
+            for(int n1 = 0; n1 < N; n1++) {
+                for(int d = 0; d < d_k; d++) {
+                    float sum = 0.0;
+                    for(int n2 = 0; n2 < N; n2++) {
+                        sum += attn_scores[h*N*N + n1*N + n2] * value[get(h,n2,d)];
+                    }
+                    result[get(h,n1,d)] = sum;
+                }
+            }
+        }
+        end = std::chrono::high_resolution_clock::now();
+        std::cout << "result: " << std::chrono::duration_cast<std::chrono::nanoseconds>(end - beg).count() << std::endl;
+
+    }
+};
+
+
+int main(void) {
+    CPUNaiveAttention cpuNaiveAttention;
+    // auto beg = std::chrono::high_resolution_clock::now();
+    cpuNaiveAttention.scaled_dot_product_attention();
+    // auto end = std::chrono::high_resolution_clock::now();
+    // auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - beg);
+    // std::cout << "duration: " << duration.count() << std::endl;
+    wrapper();
+	return 0;
+}
---
+++ src/naiveAttention.h
@@ -0,0 +1,6 @@
+#ifndef __NAIVE_ATTENTION_H__
+#define __NAIVE_ATTENTION_H__
+
+void wrapper(void);
+
+#endif
