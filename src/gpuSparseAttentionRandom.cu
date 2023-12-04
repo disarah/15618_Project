e831e3abf8d9b59f555873cae454d98a407ef9e6
soleee99 <soleeep99@gmail.com>
2023-12-03 22:49:31 -0500

    add gpu sparse attn files

---
+++ src/gpuSparseAttentionRandom.cu
@@ -0,0 +1,43 @@
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <driver_functions.h>
+#include <stdio.h>
+#include <iostream>
+#include <cmath>
+#include <chrono>
+
+#include "gpuSparseAttentionRandom.h"
+
+
+
+void gpuSparseAttentionRandom(int N, int D_MODEL, int N_HEAD) {
+    int d_k = D_MODEL / N_HEAD;
+    float sqrt_d_k = sqrt(d_k);
+
+    float *query, *key, *value, *attn_scores, *result;
+    cudaMalloc((void **)&query, N_HEAD*N*d_k * sizeof(float));
+    cudaMalloc((void **)&key, N_HEAD*N*d_k * sizeof(float));
+    cudaMalloc((void **)&value, N_HEAD*N*d_k * sizeof(float));
+    cudaMalloc((void **)&attn_scores, N_HEAD * N*N * sizeof(float));
+    cudaMalloc((void **)&result, N_HEAD*N*d_k * sizeof(float));
+
+    // =============================================================================================
+    // Tiling + random sparse
+
+    cudaEvent_t start, stop;
+    float elapsedTime = 0.0;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    cudaEventRecord(start,0);
+
+    // Kernel Here
+
+    cudaDeviceSynchronize();
+    cudaEventRecord(stop,0);
+    cudaEventSynchronize(stop);
+
+    cudaEventElapsedTime(&elapsedTime, start,stop);
+    printf("gpu random attention (tile parallelization using shared mem): %f microseconds\n\n" ,elapsedTime*1000);
+
+}
